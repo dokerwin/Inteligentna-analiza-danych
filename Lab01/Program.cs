fccb4657aa30d8e81d82bf92068a4df133e56026
Zakhar Kulbachenko <nik.name.doker@gmail.com>
2022-01-19 02:27:49 +0300

    Add recall

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -84,10 +84,10 @@ namespace Lab01
 
 
 
-                List<Article> articlesForLearning = articlesForLearning = getArticleForLearning(articles, 10);
+                List<Article> articlesForLearning = articlesForLearning = getArticleForLearning(articles, 50);
 
                 List<Article> articlesForTesting = new List<Article>();
-                Thread thread1 = new Thread(() => { articlesForTesting = getArticleForTesting(articles, 10); });
+                Thread thread1 = new Thread(() => { articlesForTesting = getArticleForTesting(articles, 50); });
                 thread1.Start();
 
                 DictionaryAllCountryCollection = CreateDictionarsFromArticles(articlesForLearning);
@@ -95,6 +95,9 @@ namespace Lab01
                 List<Vector> vectorsFromLearningArticles = Vector.getVectrorsFromArticles(articlesForLearning, DictionaryAllCountryCollection);
                 List<Vector> vectorsFromTestArticles = Vector.getVectrorsFromArticles(articlesForTesting, DictionaryAllCountryCollection);
 
+                int assd = vectorsFromTestArticles.Where(e => e.GetCountry() == "japan").Count();
+
+                Console.WriteLine("west-germany is : " +assd);
 
                 Console.WriteLine("//////////////////////////");
                 Dictionary<string, double> precis = new Dictionary<string, double>
@@ -112,6 +115,33 @@ namespace Lab01
 
                 Console.WriteLine("//////////////////////////");
                 Console.WriteLine("The best algorithm is: " + bestPrecision.Key + " Precision is: " + bestPrecision.Value);
+
+
+
+
+
+                Console.WriteLine("//////////////////////////");
+                string place = "west-germany";
+                Dictionary<string, double> recall = new Dictionary<string, double>
+                {
+                    { metrics.ChebyshevMetric.ToString(), Math.Round(GetRecall(metrics.ChebyshevMetric, vectorsFromTestArticles, vectorsFromLearningArticles, place),3) },
+
+                    {  metrics.EuclideaMetric.ToString(),  Math.Round(GetRecall(metrics.EuclideaMetric, vectorsFromTestArticles, vectorsFromLearningArticles, place),3)},
+
+                    { metrics.StreetMetric.ToString(), Math.Round(GetRecall(metrics.StreetMetric, vectorsFromTestArticles, vectorsFromLearningArticles, place) ,3)}
+
+                };
+
+                KeyValuePair<string,double> bestRecall = recall.Where(e => e.Value == recall.Max(e2 => e2.Value)).First();
+
+                Console.WriteLine("//////////////////////////");
+                Console.WriteLine("The best algorithm is: " + bestRecall.Key + " Precision is: " + bestRecall.Value);
+
+
+
+
+
+
                 //
                 //while (true)
                 //{
@@ -163,7 +193,6 @@ namespace Lab01
         private static double GetPrecision(metrics metric, List<Vector> vectorsFromTestArticles, List<Vector> vectorsFromLearningArticles)
         {
 
-
             Console.WriteLine("Used metric: " + metric.ToString());
 
          //   Console.Clear();
@@ -182,16 +211,142 @@ namespace Lab01
                 {
                     fall += 1;
                 }
-
             }
 
             double precision = correct / (correct + fall);
-
 
             Console.WriteLine("Precision of " +metric.ToString() + " is: " + precision);
             return precision;
 
         }
+
+
+
+
+        private static double GetRecall(metrics metric, List<Vector> vectorsFromTestArticles, List<Vector> vectorsFromLearningArticles, string place)
+        {
+
+            Console.WriteLine("Used metric: " + metric.ToString());
+            //   Console.Clear();
+            //Precision = TruePositives / (TruePositives + FalsePositives)
+            double TruePositive =  0;
+            double TrueNegative =  0; 
+            double FalseNegative = 0;
+       
+
+            int k = 5;
+            for (int i = 0; i < 100; i++)
+            {
+                Vector randVector = Vector.getRandomVector(vectorsFromTestArticles);
+                bool predictedCountry =  PredictCounty(metrics.EuclideaMetric, k, vectorsFromLearningArticles, randVector, place);
+                if (predictedCountry) 
+                {
+                    TruePositive += 1;
+                }
+                else if (randVector.GetCountry() == place)
+                {
+                        FalseNegative += 1;
+                }
+                else
+                {
+                    TrueNegative += 1;
+                }
+               
+
+                
+            }
+
+            double recall = TruePositive / (TruePositive + FalseNegative);
+
+            Console.WriteLine("Recal of " + metric.ToString() + " is: " + recall);
+            return recall;
+
+        }
+
+
+
+
+
+        public static bool PredictCounty(Metrics.metrics metrics, int k, List<Vector> vectorsFromLearningArticles, Vector vectorFromAnalyzise, string place)
+        {
+            List<Tuple<Vector, int>> countryMetric = new List<Tuple<Vector, int>>();
+            List<int> vectorAnalize = new List<int>();
+
+            foreach (var Abbreviation in vectorFromAnalyzise.Characteristic)
+            {
+                vectorAnalize.Add(Abbreviation.Value);
+            }
+
+            foreach (var vectors in vectorsFromLearningArticles)
+            {
+                List<int> vectorLearning = new List<int>();
+                foreach (var Abbreviation in vectors.Characteristic)
+                {
+                    vectorLearning.Add(Abbreviation.Value);
+                }
+
+                switch (metrics)
+                {
+                    case metrics.ChebyshevMetric:
+                        {
+                            var a1 = new Tuple<Vector, int>(vectors, Metrics.analysisChebyshevMetric(vectorLearning, vectorAnalize));
+                            countryMetric.Add(a1);
+                            break;
+
+                        }
+                    case metrics.EuclideaMetric:
+                        {
+                            var a2 = new Tuple<Vector, int>(vectors, Metrics.analysisEuclideaMetric(vectorLearning, vectorAnalize));
+                            countryMetric.Add(a2);
+                            break;
+
+                        }
+                    case metrics.StreetMetric:
+                        {
+                            var a3 = new Tuple<Vector, int>(vectors, Metrics.analysisStreetMetric(vectorLearning, vectorAnalize));
+                            countryMetric.Add(a3);
+                            break;
+                        }
+                }
+
+            }
+
+            Dictionary<string, int> distanceK = new Dictionary<string, int>();
+            foreach (var country in Places.All)
+            {
+                var maxNumber = countryMetric.OrderBy(x => x.Item2).Where(x => x.Item1.GetCountry() == country.Tag).Take(k); //only take 1 item
+                foreach (var m in maxNumber)
+                {
+                    if (distanceK.ContainsKey(m.Item1.GetCountry()))
+                    {
+                        if (distanceK[m.Item1.GetCountry()] > m.Item2)
+                            distanceK[m.Item1.GetCountry()] = m.Item2;
+                    }
+                    else
+                    {
+                        distanceK.Add(m.Item1.GetCountry(), m.Item2);
+                    }
+                }
+            }
+            var minDistance = distanceK.OrderBy(x => x.Value).Take(k);
+#if TEST
+            
+            Console.WriteLine("Used metric: " + metrics.ToString());
+            foreach (var m in minDistance)
+            {
+                Console.WriteLine("Country: " + m.Key + " Distance: " + m.Value);
+            }
+#endif
+            KeyValuePair<string, int> result = minDistance.Where(e => e.Value == minDistance.Min(e2 => e2.Value)).First();
+            if (result.Key == place)
+            {
+                return true;
+            }
+            else return false;
+
+        }
+
+
 
 
 
